ca54105c7e49466fb621ced016424dddeb7dbd43
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-02-21 11:55:23 +0200

    added verification bean

--- CustomBean/src/main/java/com/abalmas/dmytro/bean/TestBeanPostProcessor.java
+++ CustomBean/src/main/java/com/abalmas/dmytro/bean/TestBeanPostProcessor.java
@@ -1,8 +1,9 @@
 package com.abalmas.dmytro.bean;
 
+import static com.mysql.cj.util.Util.stackTraceToString;
+
 import com.abalmas.dmytro.annotation.RandomValue;
 import java.lang.reflect.Field;
-import lombok.SneakyThrows;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
@@ -10,15 +11,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestBeanPostProcessor implements BeanPostProcessor {
 
-  @SneakyThrows
   @Override
   public Object postProcessBeforeInitialization(Object bean, String beanName)
       throws BeansException {
-    Field[] fields = bean.getClass().getDeclaredFields();
-    for (Field field : fields) {
-      if (field.isAnnotationPresent(RandomValue.class)) {
-        field.setAccessible(true);
-        field.set(bean, Math.random());
+    if (bean instanceof TestBean) {
+      Field[] fields = bean.getClass().getDeclaredFields();
+      for (Field field : fields) {
+        if (field.isAnnotationPresent(RandomValue.class)) {
+          field.setAccessible(true);
+          try {
+            field.set(bean, Math.random());
+          } catch (IllegalArgumentException | IllegalAccessException e) {
+            throw new RuntimeException(stackTraceToString(e));
+          }
+        }
       }
     }
     return bean;
