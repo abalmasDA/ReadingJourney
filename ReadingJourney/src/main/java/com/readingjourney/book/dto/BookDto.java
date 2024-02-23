e8d919e84e61046e96395268498a9d0897e08a05
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-02-23 11:04:15 +0200

    added book dto

---
+++ ReadingJourney/src/main/java/com/readingjourney/book/dto/BookDto.java
@@ -0,0 +1,21 @@
+package com.readingjourney.book.dto;
+
+import com.readingjourney.book.entity.Author;
+import com.readingjourney.book.entity.BookDetails;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BookDto {
+
+  private String title;
+  private Long rating;
+  private Author author;
+  private BookDetails bookDetails;
+
+}
