1359b7977276425c22453ad17048f60de1262df2
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-03-05 12:29:11 +0200

    refactoring

--- ReadingJourney/src/main/java/com/readingjourney/account/configuration/SecurityWebInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.readingjourney.account.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
