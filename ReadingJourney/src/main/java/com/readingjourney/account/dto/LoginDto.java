3664a3e7ee716e1a55c73957e0fd2382d4306740
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-03-05 12:49:14 +0200

    minor refactoring

--- ReadingJourney/src/main/java/com/readingjourney/account/dto/LoginDto.java
+++ ReadingJourney/src/main/java/com/readingjourney/account/dto/LoginDto.java
@@ -5,9 +5,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginDto {
 
   @NotBlank(message = "Email is required")
