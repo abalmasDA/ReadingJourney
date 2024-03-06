2c98e200a3e38eb49c4d630caa6654bcb648582e
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-03-06 10:47:44 +0200

    minor refactoring

--- ReadingJourney/src/main/java/com/readingjourney/account/configuration/SecurityConfiguration.java
+++ ReadingJourney/src/main/java/com/readingjourney/account/configuration/SecurityConfiguration.java
@@ -9,12 +9,13 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 
 @Configuration
 @EnableWebSecurity
