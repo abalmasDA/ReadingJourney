1e2e3172b5634fc729ab723a2100bfd149486d08
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-03-11 14:28:20 +0200

    integration spring boot to project. Update dependencies

---
+++ ReadingJourney/src/main/java/com/readingjourney/ReadingJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.readingjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingJourneyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReadingJourneyApplication.class, args);
+  }
+
+}
