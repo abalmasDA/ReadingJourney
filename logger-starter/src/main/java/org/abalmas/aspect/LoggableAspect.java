d5d337163d3b3980da543caef017babe052ccbf0
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-03-12 17:08:51 +0200

    minor refactoring LoggableAspect class

--- logger-starter/src/main/java/org/abalmas/aspect/LoggableAspect.java
+++ logger-starter/src/main/java/org/abalmas/aspect/LoggableAspect.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 @Aspect
 public class LoggableAspect {
