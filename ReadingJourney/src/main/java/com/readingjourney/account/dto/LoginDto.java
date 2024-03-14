3a9fee23a0f3dc01594974d050a7afd4c98e0b8d
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-03-14 12:26:17 +0200

    minor refactoring LoginDto class: added annotation @Builder

--- ReadingJourney/src/main/java/com/readingjourney/account/dto/LoginDto.java
+++ ReadingJourney/src/main/java/com/readingjourney/account/dto/LoginDto.java
@@ -4,10 +4,12 @@ package com.readingjourney.account.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginDto {
