14c0bcea8a1a1ba9c092aa7adffa52ce2d2afc91
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-03-15 11:32:29 +0200

    Updated BookControllerTest class

--- ReadingJourney/src/test/java/com/readingjourney/controller/BookControllerTest.java
+++ ReadingJourney/src/test/java/com/readingjourney/controller/BookControllerTest.java
@@ -1,65 +1,101 @@
 package com.readingjourney.controller;
 
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.readingjourney.account.configuration.SecurityConfiguration;
+import com.readingjourney.account.jwt.JwtService;
 import com.readingjourney.book.controller.BookController;
 import com.readingjourney.book.dto.BookDto;
 import com.readingjourney.book.entity.Book;
-import com.readingjourney.book.exception.GlobalExceptionHandler;
 import com.readingjourney.book.service.BookService;
-import com.readingjourney.configuration.ApplicationConfigurationTest;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-
-@ExtendWith(SpringExtension.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = ApplicationConfigurationTest.class)
+@WebMvcTest(BookController.class)
+@Import(SecurityConfiguration.class)
 public class BookControllerTest {
 
+  @Autowired
   private MockMvc mockMvc;
 
-  @Mock
+  @MockBean
   private BookService bookService;
 
-  @InjectMocks
-  private BookController bookController;
+  @MockBean
+  private JwtService jwtService;
+
+  @MockBean
+  private UserDetailsService userDetailsService;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private final long bookId = 1;
+
+  private final long authorId = 1;
+
+  private Book book;
+
+  private BookDto bookDto;
+
+  private BookDto bookDtoUpdated;
 
   @BeforeEach
   public void setUp() {
-    MockitoAnnotations.openMocks(this);
-    mockMvc = MockMvcBuilders.standaloneSetup(bookController)
-        .setValidator(new LocalValidatorFactoryBean())
-        .setControllerAdvice(new GlobalExceptionHandler()).build();
+    book = new Book()
+        .builder()
+        .id(bookId)
+        .build();
+
+    bookDto = BookDto.builder()
+        .title("Test Book")
+        .rating(5L)
+        .yearPublication(LocalDate.of(2022, 1, 1))
+        .numberPages(200)
+        .genre("Fiction")
+        .format("Paperback")
+        .edition("First")
+        .isbn(1234567890123L)
+        .build();
+
+    bookDtoUpdated = BookDto.builder()
+        .title("Test Book Updated")
+        .rating(2L)
+        .yearPublication(LocalDate.of(2022, 1, 1))
+        .numberPages(100)
+        .genre("Non-Fiction")
+        .format("E-book")
+        .edition("First")
+        .isbn(1234567890123L)
+        .build();
+
   }
 
   @Test
+  @WithMockUser(username = "test", password = "test", roles = "USER")
   public void findAllBooksTest() throws Exception {
-    Book book = new Book();
     when(bookService.findAll()).thenReturn(Collections.singletonList(book));
     mockMvc.perform(get("/books"))
         .andExpect(status().isOk())
@@ -67,28 +103,20 @@ public class BookControllerTest {
   }
 
   @Test
+  @WithMockUser(username = "test", password = "test", roles = "USER")
   public void findBookByIdTest() throws Exception {
-    long id = 1;
-    Book book = new Book();
-    book.setId(id);
-    when(bookService.findById(id)).thenReturn(Optional.of(book));
-    mockMvc.perform(get("/books/{id}", id))
+    when(bookService.findById(bookId)).thenReturn(Optional.of(book));
+    mockMvc.perform(get("/books/{id}", bookId))
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.id").exists());
   }
 
   @Test
+  @WithMockUser(username = "test", password = "test", roles = "USER")
   public void addBookTest() throws Exception {
-    long authorId = 1;
-    long bookId = 1;
-    String requestBody = "{\"title\":\"Book Title\",\"rating\":5,"
-        + "\"yearPublication\":\"2022-01-01\",\"numberPages\":200,\"genre\":\"Fiction\","
-        + "\"format\":\"Hardcover\",\"edition\":\"First\",\"isbn\":1234567890123}";
-    Book book = new Book();
-    book.setId(bookId);
+    String requestBody = objectMapper.writeValueAsString(bookDto);
     when(bookService.save(eq(authorId), any(BookDto.class))).thenReturn(book);
-
     mockMvc.perform(post("/books/{id}", authorId)
             .contentType(MediaType.APPLICATION_JSON)
             .content(requestBody))
@@ -98,16 +126,11 @@ public class BookControllerTest {
   }
 
   @Test
+  @WithMockUser(username = "test", password = "test", roles = "USER")
   public void updateBookTest() throws Exception {
-    long id = 1;
-    String requestBody = "{\"title\":\"Updated Book Title\",\"rating\":5,"
-        + "\"yearPublication\":\"2023-01-01\",\"numberPages\":250,\"genre\":\"Non-fiction\","
-        + "\"format\":\"Paperback\",\"edition\":\"Second\",\"isbn\":9876543210987}";
-    Book book = new Book();
-    book.setId(id);
-    when(bookService.update(eq(id), any(BookDto.class))).thenReturn(book);
-
-    mockMvc.perform(put("/books/{id}", id)
+    String requestBody = objectMapper.writeValueAsString(bookDtoUpdated);
+    when(bookService.update(eq(bookId), any(BookDto.class))).thenReturn(book);
+    mockMvc.perform(put("/books/{id}", bookId)
             .contentType(MediaType.APPLICATION_JSON)
             .content(requestBody))
         .andExpect(status().isOk())
@@ -116,9 +139,9 @@ public class BookControllerTest {
   }
 
   @Test
+  @WithMockUser(username = "test", password = "test", roles = "USER")
   public void deleteBookTest() throws Exception {
-    long id = 1;
-    mockMvc.perform(delete("/books/{id}", id))
+    mockMvc.perform(delete("/books/{id}", bookId))
         .andExpect(status().isOk());
   }
 
