088d93a077f66d07c994d75ab6457b4cb001c3c4
Dmytro Abalmas <124811948+abalmasDA@users.noreply.github.com>
2024-04-02 19:51:35 +0300

    minor refactoring EmailSendingException

--- src/main/java/com/readingjourney/notification/exception/EmailSendingException.java
+++ src/main/java/com/readingjourney/notification/exception/EmailSendingException.java
@@ -1,5 +1,8 @@
 package com.readingjourney.notification.exception;
 
+/**
+ * Custom exception class for handling email sending errors.
+ */
 public class EmailSendingException extends RuntimeException {
 
   public EmailSendingException(String message) {
